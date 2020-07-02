46a58957212256d7f9388bb888900775db7858ab
Valentin <valentin.furmanek@gmail.com>
2020-07-02 12:43:12 +0200

    Add the game summary in the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { GameSummaryComponent } from './game-summary/game-summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    GameSummaryComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
+      { path: "", component: GameSummaryComponent }
     ])
     //AppRoutingModule
   ],
