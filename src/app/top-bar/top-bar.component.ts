7a7803a606fdec94acfafaedbff9bb7eb9b51462
Valentin <valentin.furmanek@gmail.com>
2020-07-05 14:34:25 +0200

    Add a language handler

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent implements OnInit {
+  toLanguage = {
+    "language": "Français",
+    "code": "fr"
+  };
 
   constructor() { }
 
   ngOnInit(): void {
+    if (window.location.hostname.split(".")[0] == "fr") {
+      this.toLanguage.language = "English";
+      this.toLanguage.code = "en";
+    } else if (window.location.hostname.split(".")[0] != "en") {
+      window.location.replace("https://fr.quatorze.valfur.fr");
+    }
   }
 
 }
